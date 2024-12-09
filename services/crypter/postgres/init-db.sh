8b1c890507ecf6a546ce8afaabd7c7059cbabb5a
falamous <falamous@mail.ru>
2024-12-09 13:02:58 +0300

    fix unique constraints

--- services/crypter/postgres/init-db.sh
+++ services/crypter/postgres/init-db.sh
@@ -5,13 +5,13 @@ psql --username crypter --dbname crypter  <<-EOSQL
 		id VARCHAR(64) PRIMARY KEY,
 		username VARCHAR(64) UNIQUE NOT NULL,
 		token VARCHAR(64) UNIQUE NOT NULL,
-		n VARCHAR UNIQUE NOT NULL
+		n VARCHAR NOT NULL
         );
 
         CREATE TABLE IF NOT EXISTS messages (
 		id VARCHAR(64) PRIMARY KEY,
-		username VARCHAR(64) UNIQUE NOT NULL,
-		from_username VARCHAR(64) UNIQUE NOT NULL,
-		encrypted VARCHAR UNIQUE NOT NULL
+		username VARCHAR(64) NOT NULL,
+		from_username VARCHAR(64) NOT NULL,
+		encrypted VARCHAR NOT NULL
         );
 EOSQL
